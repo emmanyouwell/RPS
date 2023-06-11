950e49da4e421719159c0c4598e3a3132682e96a
emmanyouwell <emingala02@gmail.com>
2023-06-11 20:42:38 +0800

    Direction

---
+++ rps.cpp
@@ -0,0 +1,17 @@
+/*
+Outline:
+1. Main menu
+2. IPO
+3. Logic (combinations)
+4. Do you want to continue?
+5. Pointing System
+
+Concepts:
+1. Nested if-else
+2. Switch statements
+3. While loop
+4. Counters
+5. Functions
+    5.1 Parameters (Pass by Ref)
+6. Trapping
+*/
